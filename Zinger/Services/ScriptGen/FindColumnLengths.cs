e082b5c59af2533dbccfbd089c035308e3c847f7
Adam O'Neil <adamosoftware@gmail.com>
2021-02-10 08:24:10 -0500

    find column max val lengths script gen

---
+++ Zinger/Services/ScriptGen/FindColumnLengths.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Data;
+
+namespace Zinger.Services.ScriptGen
+{
+    public class FindColumnLengths : ScriptGenBase
+    {
+        public override string Title => "Find Column Lengths";
+
+        public override string Sql => 
+            @"SELECT
+	            SCHEMA_NAME([tbl].[schema_id]) AS [Schema],
+	            [tbl].[name] AS [Table],
+	            [col].[name] AS [Column]
+            FROM
+	            [sys].[columns] [col]
+	            INNER JOIN [sys].[tables] [tbl] ON [col].[object_id]=[tbl].[object_id]
+            WHERE
+	            TYPE_NAME([col].[system_type_id]) LIKE '%varchar'";
+
+        protected override string GetScriptCommand(DataRow dataRow)
+        {
+            return $"SELECT '{dataRow["Schema"]}' AS [Schema], '{dataRow["Table"]}' AS [Table], '{dataRow["Column"]}' AS [Column], MAX(LEN([{dataRow["Column"]}])) AS [Length] FROM [{dataRow["Schema"]}].[{dataRow["Table"]}]";
+        }
+    }
+}
