d661a78ec3e86ccedc9d2ae4e95054c8e0607abd
Adam O'Neil <adamosoftware@gmail.com>
2021-02-19 21:19:45 -0500

    started on data migration feature

---
+++ Zinger/Models/DataMigration.cs
@@ -0,0 +1,26 @@
+﻿namespace Zinger.Models
+{
+    public class DataMigration
+    {
+        public string SourceConnection { get; set; }
+        public string DestConnection { get; set; }
+
+        public class Step
+        {
+            public int Order { get; set; }
+            public string FromSql { get; set; }
+            public string IntoTable { get; set; } // use with Column.KeyMapTable
+            public string IdentityColumn { get; set; }
+            public Column[] Columns { get; set; }
+        }
+
+        public class Column
+        {
+            public string Source { get; set; }
+            public string Dest { get; set; }
+            public string KeyMapTable { get; set; }
+        }
+    }
+
+
+}
