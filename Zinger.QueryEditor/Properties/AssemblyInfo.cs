8a0b1484be29ef230e0e0f006344cc40dc163000
Adam Oneil <adamsoftware@gmail.com>
2021-06-19 18:46:25 -0400

    seems to be working

---
+++ Zinger.QueryEditor/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("QueryEditor")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("QueryEditor")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("55f2c937-19f3-4b11-92f7-bb3614402fad")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- Zinger/Forms/frmQuery.Designer.cs
+++ Zinger/Forms/frmQuery.Designer.cs
@@ -36,18 +36,18 @@
             this.btnDataToScript = new System.Windows.Forms.ToolStripButton();
             this.btnSchema = new System.Windows.Forms.ToolStripButton();
             this.btnImportExcel = new System.Windows.Forms.ToolStripButton();
+            this.btnScriptGen = new System.Windows.Forms.ToolStripDropDownButton();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.queryEditor1 = new Zinger.Controls.QueryEditor();
             this.tabPage2 = new System.Windows.Forms.TabPage();
             this.resultClassBuilder1 = new Zinger.Controls.ResultClassBuilder();
             this.splcQueryAndSourceTree = new System.Windows.Forms.SplitContainer();
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
-            this.schemaBrowser1 = new Zinger.Controls.SchemaBrowser();
             this.tabNavigation = new System.Windows.Forms.TabControl();
             this.tabPage3 = new System.Windows.Forms.TabPage();
             this.tabPage4 = new System.Windows.Forms.TabPage();
-            this.btnScriptGen = new System.Windows.Forms.ToolStripDropDownButton();
+            this.queryEditor1 = new Zinger.Controls.QueryEditor();
+            this.schemaBrowser1 = new Zinger.Controls.SchemaBrowser();
             this.toolStrip1.SuspendLayout();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
@@ -136,6 +136,16 @@
             this.btnImportExcel.Text = "Import Excel...";
             this.btnImportExcel.Click += new System.EventHandler(this.btnImportExcel_Click);
             // 
+            // btnScriptGen
+            // 
+            this.btnScriptGen.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.btnScriptGen.Enabled = false;
+            this.btnScriptGen.Image = ((System.Drawing.Image)(resources.GetObject("btnScriptGen.Image")));
+            this.btnScriptGen.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnScriptGen.Name = "btnScriptGen";
+            this.btnScriptGen.Size = new System.Drawing.Size(74, 22);
+            this.btnScriptGen.Text = "Script Gen";
+            // 
             // tabControl1
             // 
             this.tabControl1.Controls.Add(this.tabPage1);
@@ -145,7 +155,7 @@
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.Padding = new System.Drawing.Point(10, 5);
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(779, 461);
+            this.tabControl1.Size = new System.Drawing.Size(529, 461);
             this.tabControl1.TabIndex = 1;
             // 
             // tabPage1
@@ -154,26 +164,10 @@
             this.tabPage1.Location = new System.Drawing.Point(4, 26);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(771, 431);
+            this.tabPage1.Size = new System.Drawing.Size(521, 431);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "SQL";
             this.tabPage1.UseVisualStyleBackColor = true;
-            // 
-            // queryEditor1
-            // 
-            this.queryEditor1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.queryEditor1.Enabled = false;
-            this.queryEditor1.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.queryEditor1.Location = new System.Drawing.Point(3, 3);
-            this.queryEditor1.Name = "queryEditor1";
-            this.queryEditor1.Parameters = null;
-            this.queryEditor1.Provider = null;
-            this.queryEditor1.QueryName = null;
-            this.queryEditor1.Size = new System.Drawing.Size(765, 425);
-            this.queryEditor1.Sql = "";
-            this.queryEditor1.TabIndex = 0;
-            this.queryEditor1.Executed += new System.EventHandler(this.queryEditor1_Executed);
-            this.queryEditor1.Modified += new System.EventHandler(this.queryEditor1_Modified);
             // 
             // tabPage2
             // 
@@ -181,7 +175,7 @@
             this.tabPage2.Location = new System.Drawing.Point(4, 26);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(717, 431);
+            this.tabPage2.Size = new System.Drawing.Size(771, 431);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "C#";
             this.tabPage2.UseVisualStyleBackColor = true;
@@ -195,7 +189,7 @@
             this.resultClassBuilder1.QueryClass = "";
             this.resultClassBuilder1.QueryName = "";
             this.resultClassBuilder1.ResultClass = "";
-            this.resultClassBuilder1.Size = new System.Drawing.Size(711, 425);
+            this.resultClassBuilder1.Size = new System.Drawing.Size(765, 425);
             this.resultClassBuilder1.TabIndex = 0;
             this.resultClassBuilder1.QueryNameChanged += new System.EventHandler(this.resultClassBuilder1_QueryNameChanged);
             // 
@@ -208,6 +202,7 @@
             // 
             // splcQueryAndSourceTree.Panel1
             // 
+            this.splcQueryAndSourceTree.Panel1.BackColor = System.Drawing.SystemColors.Control;
             this.splcQueryAndSourceTree.Panel1.Controls.Add(this.splitContainer1);
             // 
             // splcQueryAndSourceTree.Panel2
@@ -232,19 +227,9 @@
             // splitContainer1.Panel2
             // 
             this.splitContainer1.Panel2.Controls.Add(this.schemaBrowser1);
-            this.splitContainer1.Panel2Collapsed = true;
             this.splitContainer1.Size = new System.Drawing.Size(779, 461);
-            this.splitContainer1.SplitterDistance = 493;
+            this.splitContainer1.SplitterDistance = 529;
             this.splitContainer1.TabIndex = 2;
-            // 
-            // schemaBrowser1
-            // 
-            this.schemaBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.schemaBrowser1.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.schemaBrowser1.Location = new System.Drawing.Point(0, 0);
-            this.schemaBrowser1.Name = "schemaBrowser1";
-            this.schemaBrowser1.Size = new System.Drawing.Size(96, 100);
-            this.schemaBrowser1.TabIndex = 0;
             // 
             // tabNavigation
             // 
@@ -277,20 +262,34 @@
             this.tabPage4.Text = "Source Files";
             this.tabPage4.UseVisualStyleBackColor = true;
             // 
-            // btnScriptGen
+            // queryEditor1
             // 
-            this.btnScriptGen.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
-            this.btnScriptGen.Enabled = false;
-            this.btnScriptGen.Image = ((System.Drawing.Image)(resources.GetObject("btnScriptGen.Image")));
-            this.btnScriptGen.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.btnScriptGen.Name = "btnScriptGen";
-            this.btnScriptGen.Size = new System.Drawing.Size(74, 22);
-            this.btnScriptGen.Text = "Script Gen";
+            this.queryEditor1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.queryEditor1.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.queryEditor1.Location = new System.Drawing.Point(3, 3);
+            this.queryEditor1.Name = "queryEditor1";
+            this.queryEditor1.Provider = null;
+            this.queryEditor1.QueryName = null;
+            this.queryEditor1.Size = new System.Drawing.Size(515, 425);
+            this.queryEditor1.Sql = "";
+            this.queryEditor1.TabIndex = 1;
+            this.queryEditor1.Executed += new System.EventHandler(this.queryEditor1_Executed);
+            this.queryEditor1.JoinResolutionRequested += new System.EventHandler<string>(this.QueryEditor1_JoinResolutionRequested);
+            // 
+            // schemaBrowser1
+            // 
+            this.schemaBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.schemaBrowser1.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.schemaBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.schemaBrowser1.Name = "schemaBrowser1";
+            this.schemaBrowser1.Size = new System.Drawing.Size(246, 461);
+            this.schemaBrowser1.TabIndex = 0;
             // 
             // frmQuery
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
             this.ClientSize = new System.Drawing.Size(779, 486);
             this.Controls.Add(this.splcQueryAndSourceTree);
             this.Controls.Add(this.toolStrip1);
@@ -330,18 +329,18 @@
         private System.Windows.Forms.SplitContainer splcQueryAndSourceTree;
         private System.Windows.Forms.TabControl tabNavigation;
         private System.Windows.Forms.TabPage tabPage3;
-        private System.Windows.Forms.TabPage tabPage4;
-        private Controls.QueryEditor queryEditor1;
+        private System.Windows.Forms.TabPage tabPage4;        
         private System.Windows.Forms.ToolStripButton btnConnections;
         private System.Windows.Forms.ToolStripComboBox cbConnection;
         private System.Windows.Forms.ToolStripButton btnRunQuery;
 		private Controls.ResultClassBuilder resultClassBuilder1;
         private System.Windows.Forms.ToolStripButton btnDataToScript;
         private System.Windows.Forms.ToolStripButton btnSchema;
-        private System.Windows.Forms.SplitContainer splitContainer1;
-        private Controls.SchemaBrowser schemaBrowser1;
+        private System.Windows.Forms.SplitContainer splitContainer1;        
         private System.Windows.Forms.ToolStripButton btnImportExcel;
         private System.Windows.Forms.ToolStripDropDownButton btnScriptGen;
+        private Controls.QueryEditor queryEditor1;
+        private Controls.SchemaBrowser schemaBrowser1;
     }
 }
 
