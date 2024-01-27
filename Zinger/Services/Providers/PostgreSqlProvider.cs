2e6820ba2e70c7a10bb8dffffc7a93dd1e25493a
Adam O'Neil <adamosoftware@gmail.com>
2024-01-27 06:34:44 -0500

    why this virtual method

--- Zinger/Services/Providers/PostgreSqlProvider.cs
+++ Zinger/Services/Providers/PostgreSqlProvider.cs
@@ -18,6 +18,11 @@ namespace Zinger.Services.Providers
 
         protected override IDbDataAdapter GetAdapter(IDbCommand command) => new NpgsqlDataAdapter(command as NpgsqlCommand);
 
+        /// <summary>
+        /// Unlike other ADO providers, Postgres doesn't do implicit type conversions on param
+        /// values to their target DbTypes. It complains if you try to assign a string to an int,
+        /// for instance, so I do this explicitly.
+        /// </summary>
         public override object ConvertParamValue(object @object, DbType dbType)
         {
             if (dbType == DbType.Int32) return Convert.ToInt32(@object);
